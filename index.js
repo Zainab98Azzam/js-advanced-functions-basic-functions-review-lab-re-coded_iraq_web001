64ce577006e62964eea2a6800495940cfc692ebf
Zainab Azzam <zainab_azzam@yahoo.com>
2020-07-06 12:54:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun (a="roller-skate"){
+   console.log(`This Saturday, I want to ${a}`)
+  }
+  saturdayFun()
+  saturdayFun("bathe my dog!")
+  
+function mondayWork(a = "go to the office"){
+  
+  console.log(`This Monday, I will ${a}`)
+}
+
+function wrapAdjective (b="*"){
+  return function(a="special"){
+    return console.log(`You are ${a}`)
+  }
+}
